15f58fbacc8d24b26f9adf4b7fed5f05b6acee89
Joe Sarsfield <joe.sarsfield@gmail.com>
2021-05-19 00:41:02 +0100

    mlflow fix

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/bash.bashrc
-mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --file-store ./mlruns --host 0.0.0.0 &
+mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --host 0.0.0.0 &
 jupyter lab --notebook-dir=/tf --ip 0.0.0.0 --no-browser --allow-root
